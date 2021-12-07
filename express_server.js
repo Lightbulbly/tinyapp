0dcacd9ac2c7943e9474d27b644cd09019e8cf63
Noya Onggud <yanuo44@gmail.com>
2021-12-06 21:43:37 -0500

    sent Data to urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -24,7 +24,14 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
- 
+
+app.get("/urls", (req, res) => {
+  // const templateVars = { greeting: 'Hello World!' };
+  // res.render("urls_index", templateVars);
+  res.render("urls_index", {urls: urlDatabase});
+
+});
+
 // app.get("/set", (req, res) => {
 //   const a = 1;
 //   res.send(`a = ${a}`);
